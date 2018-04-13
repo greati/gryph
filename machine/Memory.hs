1ec29b703634ba90ec3ede8a2528059571abe0bc
Vitor Greati <greati@ufrn.edu.br>
2018-04-13 00:17:59 -0300

    Include more on memory

--- machine/Memory.hs
+++ machine/Memory.hs
@@ -1,10 +1,17 @@
 module Memory where
 
-
 data TypeName   = GInt | GFloat | GChar | GString | GBool | GList deriving (Show)
 data Value      = Integer Integer | Float Double | Char Char | String String | Bool Bool | List [Value] deriving (Show)
-data Cell       = Cell (String, String, Value) deriving (Show)
+type Cell       = (String, String, Value)
+
+type MemoryState = [Cell]
 
 getType :: Value -> TypeName
 getType x = case x of
                 Integer _ -> GInt
+
+insert :: Cell -> MemoryState -> MemoryState
+insert k' [] = [k']
+insert k'@(k,_,v) (x'@(x,y,z):xs)
+    | k == x   = (k,y,v):xs
+    | otherwise = x' : insert k' xs 
