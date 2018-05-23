fd1d4a82008761de65672f588e558d0983b643f3
Vinicius Campos <viniciuscampos120@gmail.com>
2018-05-23 18:38:10 -0300

    Start the implementation of control flow

---
+++ interpreter/src/Execution/RepetitionSemantic.hs
@@ -0,0 +1,14 @@
+module Execution.Repetition where
+
+import Data.List
+
+over :: [[a]] -> [[a]]
+over [[]]         = [[]] 
+over [xs]         = [[x] | x <- xs]
+over ([x]:xss)    = (join x (over xss))
+over ((x:xs):xss) = (join x (over xss)) ++ (over ((xs):xss))
+
+join i [[]]   = [[]]
+join i [x]    = [(i : x)]
+join i (x:xs) = (i : x) : (join i xs)
+
