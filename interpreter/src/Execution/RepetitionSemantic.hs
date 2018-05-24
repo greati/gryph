fa9a9c35704edeb687958293b77df761a8e692ac
Vinícius Campos <viniciuscampos120@gmail.com>
2018-05-24 02:02:18 -0300

    Implement the basic idea of for statement

--- interpreter/src/Execution/RepetitionSemantic.hs
+++ interpreter/src/Execution/RepetitionSemantic.hs
@@ -1,14 +1,38 @@
-module Execution.Repetition where
+module Execution.RepetitionSemantic where
 
-import Data.List
+import Syntactic.Syntax          as Sy
+import qualified Data.List       as L
+import qualified Data.Map.Strict as M
 
 over :: [[a]] -> [[a]]
 over [[]]         = [[]] 
 over [xs]         = [[x] | x <- xs]
 over ([x]:xss)    = (join x (over xss))
-over ((x:xs):xss) = (join x (over xss)) ++ (over ((xs):xss))
+over ((x:xs):xss) = (join x (over xss)) L.++ (over ((xs):xss))
 
 join i [[]]   = [[]]
 join i [x]    = [(i : x)]
 join i (x:xs) = (i : x) : (join i xs)
 
+--for :: [String] -> [[a]] -> IO ()
+for is xss = do 
+    let (ys:yss) = over xss
+    -- Memory simulation
+    let lt       = zip is ys
+    let memory   = M.fromList lt
+    return (for' is yss memory)
+
+for' is xss memory = do 
+    if xss /= [] 
+    then do
+        -- Execute something
+        let memory' = updateMemory is (L.head xss) memory
+        (for' is (L.tail xss) memory')
+    else do
+        (memory)
+    
+
+updateMemory :: Ord a => [a] -> [b] -> (M.Map a b) -> (M.Map a b)
+updateMemory [x] [y] mem       = M.insert x y mem
+updateMemory (x:xs) (y:ys) mem = updateMemory xs ys (M.insert x y mem)
+     
--- interpreter/src/Main.hs
+++ interpreter/src/Main.hs
@@ -5,6 +5,7 @@ import Syntactic.Syntax
 import Syntactic.Values
 import Execution.Semantic
 import Execution.Memory
+import Execution.RepetitionSemantic
 
 main :: IO ()
 main = undefined
