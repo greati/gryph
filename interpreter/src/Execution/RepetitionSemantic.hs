7d8fc1e6271b0031628315f3c7a274af6b150a96
Vinicius Campos <viniciuscampos120@gmail.com>
2018-05-24 14:47:23 -0300

    Finish the basic version of command for

--- interpreter/src/Execution/RepetitionSemantic.hs
+++ interpreter/src/Execution/RepetitionSemantic.hs
@@ -15,22 +15,36 @@ join i [x]    = [(i : x)]
 join i (x:xs) = (i : x) : (join i xs)
 
 --for :: [String] -> [[a]] -> IO ()
-for is xss = do 
+for is xss exp = do 
     let (ys:yss) = over xss
     -- Memory simulation
     let lt       = zip is ys
     let memory   = M.fromList lt
-    return (for' is yss memory)
+    for' is yss memory exp
+    return ()
 
-for' is xss memory = do 
+for' is xss memory exp = do 
     if xss /= [] 
     then do
-        -- Execute something
-        let memory' = updateMemory is (L.head xss) memory
-        (for' is (L.tail xss) memory')
-    else do
-        (memory)
+        if exp == True
+        then do
+            putStrLn (show memory)
+            
+            -- Execute something
+            
+            let memory' = updateMemory is (L.head xss) memory
+            (for' is (L.tail xss) memory' exp)
+        
+        else do
+            let memory' = updateMemory is (L.head xss) memory
+            (for' is (L.tail xss) memory' exp)
     
+    else do
+        if exp == True
+        then do 
+            putStrLn (show memory)
+            return ()
+        else do return () 
 
 updateMemory :: Ord a => [a] -> [b] -> (M.Map a b) -> (M.Map a b)
 updateMemory [x] [y] mem       = M.insert x y mem
