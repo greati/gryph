c9fb646426eccb70f9c483cc3f39ea989aa918e9
Vinícius Campos <viniciuscampos120@gmail.com>
2018-05-25 00:16:01 -0300

    Implement dfs

--- interpreter/src/Execution/GraphSemantic.hs
+++ interpreter/src/Execution/GraphSemantic.hs
@@ -44,6 +44,44 @@ bfs' g v queue vis = do
                 bfs' g (L.head n_queue) (L.tail n_queue) n_vis 
                 return ()
 
+dfs :: (Show a, Show b) => G.Graph a b -> G.Vertex a -> IO ()
+dfs g v = do
+    dfs' g v [] S.empty
+    return ()
+
+
+dfs' :: (Show a, Show b) => G.Graph a b -> Vertex a -> [Vertex a] -> S.Set (Vertex a) -> IO ()
+dfs' g v stack vis = do
+    if S.member v vis
+    then do
+        if stack == []
+        then do
+            return ()
+        else do
+            if L.length stack == 1
+            then do
+                bfs' g (L.head stack) [] vis
+                return ()
+            else do
+                bfs' g (L.head stack) (L.tail stack) vis
+                return ()
+    else do
+        putStrLn (show v)
+        let n_vis    = S.insert v vis
+        let n_stack  = (verticesOfEdges (G.getEdges g v)) ++ stack
+
+        if n_stack == []
+        then do
+            return ()
+        else do
+            if L.length n_stack == 1
+            then do
+                bfs' g (L.head stack) [] vis
+                return () 
+            else do
+                bfs' g (L.head n_stack) (L.tail n_stack) n_vis 
+                return ()
+
 verticesOfEdges :: [G.Edge a b] -> [Vertex a]
 verticesOfEdges [] = []
 verticesOfEdges [G.Edge v1 v2 _] =  [v2]
