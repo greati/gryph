fa69e458a4a7abee3eba21ab85ac854d966c3be0
Vinícius Campos <viniciuscampos120@gmail.com>
2018-05-31 23:29:51 -0300

    Start the list comprehension

--- interpreter/src/Execution/RepetitionSemantic.hs
+++ interpreter/src/Execution/RepetitionSemantic.hs
@@ -1,6 +1,9 @@
 module Execution.RepetitionSemantic where
 
-import Syntactic.Syntax          as Sy
+import Syntactic.Syntax
+import Syntactic.Values
+import Execution.Memory
+import Execution.Semantic
 import qualified Data.List       as L
 import qualified Data.Map.Strict as M
 
@@ -70,4 +73,27 @@ for' is xss memory exp = do
 updateMemory :: Ord a => [a] -> [b] -> (M.Map a b) -> (M.Map a b)
 updateMemory [x] [y] mem       = M.insert x y mem
 updateMemory (x:xs) (y:ys) mem = updateMemory xs ys (M.insert x y mem)
-     
+
+list_comp  = (ListComp (ArithBinExpr PlusBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (IdTerm (Ident "b")))) (ForIterator [Ident "a",Ident "b"] [ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 1))),ArithTerm (LitTerm (Lit (Integer 2))),ArithTerm (LitTerm (Lit (Integer 3)))]),ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 5))),ArithTerm (LitTerm (Lit (Integer 6))),ArithTerm (LitTerm (Lit (Integer 7)))])] [ArithRelExpr Less (ArithBinExpr TimesBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (LitTerm (Lit (Integer 5))))) (ArithTerm (LitTerm (Lit (Integer 10))))]))
+list_comp' = (ListComp (ArithBinExpr PlusBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (IdTerm (Ident "b")))) (ForIterator [Ident "a"] [ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 1))),ArithTerm (LitTerm (Lit (Integer 2))),ArithTerm (LitTerm (Lit (Integer 3)))])] [ArithRelExpr Less (ArithBinExpr TimesBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (LitTerm (Lit (Integer 5))))) (ArithTerm (LitTerm (Lit (Integer 10))))]))
+
+overListComp :: [Value] -> [Value]
+overListComp [EmptyList]         = [EmptyList] 
+overListComp [(List xs)]         = [(List [x]) | x <- xs]
+overListComp (List [x]: xss)   = joinListComp x (overListComp xss)
+overListComp (List (x:xs):xss) = (joinListComp x (overListComp xss)) ++ (overListComp ((List xs): xss))
+
+
+joinListComp :: Value -> [Value] -> [Value]
+joinListComp v [EmptyList]     = [EmptyList]
+joinListComp v [(List xs)]     = [ List (v : xs) ]
+joinListComp v ((List xs):xss) = (List (v : xs)) : (joinListComp v xss)
+
+
+evalListComp :: Memory -> ProgramMemory -> Scopes -> ListComp -> (ArithExpr, [Identifier], [Value], ArithExpr)
+evalListComp m pm ss (ListComp expression (ForIterator is xs [when_exp] ) ) = (expression, is, overListComp (getLists [xs]), when_exp)
+    where
+        getLists (xs:[])  = (evalList m pm ss xs)
+        getLists (xs:xss) = (evalList m pm ss xs) ++ getLists xss
+
+
