189c1ccc8b5c414a11af1efea0f080d80023c3c7
Vinícius Campos <viniciuscampos120@gmail.com>
2018-06-01 03:12:59 -0300

    Continue the implementation of list comprehension

--- interpreter/src/Execution/RepetitionSemantic.hs
+++ interpreter/src/Execution/RepetitionSemantic.hs
@@ -77,23 +77,56 @@ updateMemory (x:xs) (y:ys) mem = updateMemory xs ys (M.insert x y mem)
 list_comp  = (ListComp (ArithBinExpr PlusBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (IdTerm (Ident "b")))) (ForIterator [Ident "a",Ident "b"] [ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 1))),ArithTerm (LitTerm (Lit (Integer 2))),ArithTerm (LitTerm (Lit (Integer 3)))]),ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 5))),ArithTerm (LitTerm (Lit (Integer 6))),ArithTerm (LitTerm (Lit (Integer 7)))])] [ArithRelExpr Less (ArithBinExpr TimesBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (LitTerm (Lit (Integer 5))))) (ArithTerm (LitTerm (Lit (Integer 10))))]))
 list_comp' = (ListComp (ArithBinExpr PlusBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (IdTerm (Ident "b")))) (ForIterator [Ident "a"] [ExprLiteral (ListLit [ArithTerm (LitTerm (Lit (Integer 1))),ArithTerm (LitTerm (Lit (Integer 2))),ArithTerm (LitTerm (Lit (Integer 3)))])] [ArithRelExpr Less (ArithBinExpr TimesBinOp (ArithTerm (IdTerm (Ident "a"))) (ArithTerm (LitTerm (Lit (Integer 5))))) (ArithTerm (LitTerm (Lit (Integer 10))))]))
 
-overListComp :: [Value] -> [Value]
-overListComp [EmptyList]         = [EmptyList] 
-overListComp [(List xs)]         = [(List [x]) | x <- xs]
-overListComp (List [x]: xss)   = joinListComp x (overListComp xss)
-overListComp (List (x:xs):xss) = (joinListComp x (overListComp xss)) ++ (overListComp ((List xs): xss))
+overListComp :: [Value] -> IO [Value]
+overListComp [EmptyList]       = do return [EmptyList] 
+overListComp [(List xs)]       = do return [(List [x]) | x <- xs]
+overListComp (List [x]: xss)   = do xss' <- (overListComp xss)
+                                    xss'' <- (joinListComp x xss')
+                                    return xss''
+overListComp (List (x:xs):xss) = do xss'  <- (overListComp xss)
+                                    xss'' <- (joinListComp x xss') 
+                                    xss''' <- (overListComp ((List xs): xss))
+                                    return (xss'' ++ xss''')
 
+joinListComp :: Value -> [Value] -> IO [Value]
+joinListComp v [EmptyList]     = do return [EmptyList]
+joinListComp v [(List xs)]     = do return [ List (v : xs) ]
+joinListComp v ((List xs):xss) = do xss' <- (joinListComp v xss)
+                                    return ((List (v : xs)) : xss')
 
-joinListComp :: Value -> [Value] -> [Value]
-joinListComp v [EmptyList]     = [EmptyList]
-joinListComp v [(List xs)]     = [ List (v : xs) ]
-joinListComp v ((List xs):xss) = (List (v : xs)) : (joinListComp v xss)
+evalListComp :: Memory -> ProgramMemory -> Scopes -> ListComp -> IO (ArithExpr, [Identifier], [Value], ArithExpr)
+evalListComp m pm ss (ListComp expression (ForIterator is xs [when_exp] ) ) = do
+        xss <- (getLists m pm ss [xs])
+        xss' <- (overListComp xss)
+        return (expression, is, xss', when_exp)
+    where getLists m pm ss (xs:[])  = do xss <- (evalList m pm ss xs)
+                                         return xss
+          getLists m pm ss (xs:xss) = do xss' <- (evalList m pm ss xs) 
+                                         xss'' <- (getLists m pm ss xss)
+                                         return (xss' ++ xss'')
 
+forListComp :: Memory -> ProgramMemory -> Scopes -> ListComp -> IO [Value]
+forListComp m pm ss lc = do (exp, id, vs'@(v:vs), when_exp) <- evalListComp m pm ss lc
+                            return [EmptyList]
 
-evalListComp :: Memory -> ProgramMemory -> Scopes -> ListComp -> (ArithExpr, [Identifier], [Value], ArithExpr)
-evalListComp m pm ss (ListComp expression (ForIterator is xs [when_exp] ) ) = (expression, is, overListComp (getLists [xs]), when_exp)
-    where
-        getLists (xs:[])  = (evalList m pm ss xs)
-        getLists (xs:xss) = (evalList m pm ss xs) ++ getLists xss
+forListComp' :: Memory -> ProgramMemory -> Scope -> ArithExpr -> [Identifier] -> [Value] -> ArithExpr -> IO [Value]
+forListComp' m pm ss exp id [v] when_exp = do
+                                                (Right m') <- updateListIds m [ss] id v
+                                                success <- (eval m' pm [ss] when_exp)
+                                                if success == (Bool True)
+                                                then do
+                                                    e <- eval m' pm [ss] exp 
+                                                    return [e]
+                                                else do 
+                                                    return [EmptyList]
 
+forListComp' m pm ss exp id (v:vs) when_exp = undefined   
 
+updateListIds :: Memory -> Scopes -> [Identifier] -> Value -> IO (Either String Memory)
+updateListIds m ss [(Ident id)] (List [v])        = do 
+                                                        let r = updateVar m id ss ((getType v), (Value v))
+                                                        return r
+updateListIds m ss ((Ident id):ids) (List (v:vs)) = do
+                                                        let (Right m') = updateVar m id ss ((getType v), (Value v))
+                                                        r <- updateListIds m' ss ids (List vs)
+                                                        return r
