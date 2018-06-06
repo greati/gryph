a4b74a32d16444d3f20d0f438ba00aa4cfe92e8a
Artur <arturmcuringa@gmail.com>
2018-06-06 11:49:24 -0300

    Fix Integer relational operation with Float

--- interpreter/src/Syntactic/Values.hs
+++ interpreter/src/Syntactic/Values.hs
@@ -17,7 +17,72 @@ data Value =    Integer Integer |
                 Map (M.Map Value  Value) |
                 EmptyMap |
 		Graph (G.Graph Value Value) 
-                deriving (Eq, Ord)
+
+instance Eq Value where
+    (==) (Integer i) (Integer i2)       = i == i2 
+    (==) (Float f) (Float f2)           = f == f2 
+    (==) (Float f) (Integer i)          = f == fromInteger i  
+    (==) (Integer i) (Float f)          = fromInteger i == f
+    (==) (Char c) (Char c2)             = c == c2 
+    (==) (String s) (String s2)         = s == s2 
+    (==) (Bool b) (Bool b2)             = b == b2 
+    (==) (List l1) (List l2)            = l1 == l2 
+    (==) (EmptyList) (EmptyList)        = EmptyList ==  EmptyList 
+    (==) (Pair p1) (Pair p2)            = p1 == p2 
+    (==) (Triple t1) (Triple t2)        = t1 == t2 
+    (==) (Quadruple q1) (Quadruple q2)  = q1 == q2 
+    (==) (Map m1) (Map m2)              = m1 == m2
+    (==) (EmptyMap) (EmptyMap)          = EmptyMap == EmptyMap
+    (==) (Graph g1) (Graph g2)          = g1 == g2
+
+instance Ord Value where
+    (<=) (Integer i) (Integer i2)       = i <= i2 
+    (<=) (Float f) (Float f2)           = f <= f2 
+    (<=) (Float f) (Integer i)          = f <= fromInteger i  
+    (<=) (Integer i) (Float f)          = fromInteger i <= f
+    (<=) (Char c) (Char c2)             = c <= c2 
+    (<=) (String s) (String s2)         = s <= s2 
+    (<=) (Bool b) (Bool b2)             = b <= b2 
+    (<=) (List l1) (List l2)            = l1 <= l2 
+    (<=) (EmptyList) (EmptyList)        = EmptyList <=  EmptyList 
+    (<=) (Pair p1) (Pair p2)            = p1 <= p2 
+    (<=) (Triple t1) (Triple t2)        = t1 <= t2 
+    (<=) (Quadruple q1) (Quadruple q2)  = q1 <= q2 
+    (<=) (Map m1) (Map m2)              = m1 <= m2
+    (<=) (EmptyMap) (EmptyMap)          = EmptyMap <= EmptyMap
+    (<=) (Graph g1) (Graph g2)          = g1 <= g2
+
+    (<) (Integer i) (Integer i2)       = i < i2 
+    (<) (Float f) (Float f2)           = f < f2 
+    (<) (Float f) (Integer i)          = f < fromInteger i  
+    (<) (Integer i) (Float f)          = fromInteger i < f
+    (<) (Char c) (Char c2)             = c < c2 
+    (<) (String s) (String s2)         = s < s2 
+    (<) (Bool b) (Bool b2)             = b < b2 
+    (<) (List l1) (List l2)            = l1 < l2 
+    (<) (EmptyList) (EmptyList)        = EmptyList <  EmptyList 
+    (<) (Pair p1) (Pair p2)            = p1 < p2 
+    (<) (Triple t1) (Triple t2)        = t1 < t2 
+    (<) (Quadruple q1) (Quadruple q2)  = q1 < q2 
+    (<) (Map m1) (Map m2)              = m1 < m2
+    (<) (EmptyMap) (EmptyMap)          = EmptyMap < EmptyMap
+    (<) (Graph g1) (Graph g2)          = g1 < g2
+
+    (>) (Integer i) (Integer i2)       = i > i2 
+    (>) (Float f) (Float f2)           = f > f2 
+    (>) (Float f) (Integer i)          = f > fromInteger i  
+    (>) (Integer i) (Float f)          = fromInteger i > f
+    (>) (Char c) (Char c2)             = c > c2 
+    (>) (String s) (String s2)         = s > s2 
+    (>) (Bool b) (Bool b2)             = b > b2 
+    (>) (List l1) (List l2)            = l1 > l2 
+    (>) (EmptyList) (EmptyList)        = EmptyList >  EmptyList 
+    (>) (Pair p1) (Pair p2)            = p1 > p2 
+    (>) (Triple t1) (Triple t2)        = t1 > t2 
+    (>) (Quadruple q1) (Quadruple q2)  = q1 > q2 
+    (>) (Map m1) (Map m2)              = m1 > m2
+    (>) (EmptyMap) (EmptyMap)          = EmptyMap > EmptyMap
+    (>) (Graph g1) (Graph g2)          = g1 > g2
 
 instance Show Value where
     show (Integer x)   = show x
